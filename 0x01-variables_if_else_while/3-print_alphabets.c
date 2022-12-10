b61e5fb1c933754ed18c715382a390a8c1708076
BkayOne <chiedoziefrankelly@gmail.com>
2022-12-10 01:34:07 -0800

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char lower = 'a';
+	char upper = 'A';
+
+	while (lower <= 'z')
+	{
+		putchar(lower);
+		lower++;
+	}
+	while (upper <= 'z')
+	{
+		putchar(upper);
+		upper++;
+	}
+	putchar('\n');
+
+	return (0);
+}
