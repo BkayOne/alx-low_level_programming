063b80dc6ef3e2951cbb0f42661b2ffa9f13aba6
BkayOne <chiedoziefrankelly@gmail.com>
2022-12-18 13:59:07 -0800

    2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -15,16 +15,16 @@ void print_alphabet_x10(void)
 	int i = 0;
 
 	while (i < 10)
-        {
+ 	{
 		character = 'a';
 
 		while (character <= 'z')
 		{
-																	        _putchar(character);
-																		character++;
-																	}
+			_putchar(character);
+			character++;
+		}
 		_putchar('\n');
-															                i++;
-																}
+		i++;
+	}
 
 }
