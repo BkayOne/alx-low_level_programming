718ff3a5616ac34168c24073ea79573b1946eb46
BkayOne <chiedoziefrankelly@gmail.com>
2023-01-04 05:28:09 -0800

    Initializing task 0

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int factorial(int n);
+int _strlen_recursion(char *s);
+int _pow_recursion(int x, int y);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int helperf(int c, int i);
+int is_prime_number(int n);
+int helperf2(int n, int d);
+int _sqrt(int x, int i);
+int is_palindrome(char *s);
+int comparator(char *s, int n1, int n2);
+int wildcmp(char *s1, char *s2);
+
+
+
+#define MAIN_H
+#endif /*MAIN_H*/
