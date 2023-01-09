d6aa53590251403db6a8e54308688e8db025c89e
BkayOne <chiedoziefrankelly@gmail.com>
2023-01-09 13:30:05 -0800

    Task 0 memory allocation

--- 0-main.c
+++ 0-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * simple_print_buffer - prints buffer in hexa
@@ -17,7 +18,7 @@ void simple_print_buffer(char *buffer, unsigned int size)
 	{
 		if (i % 10)
 		{
-			printf(" ");
+		    printf(" ");
 		}
 		if (!(i % 10) && i)
 		{
@@ -30,17 +31,21 @@ void simple_print_buffer(char *buffer, unsigned int size)
 }
 
 /**
- * main - check the code
+ * main - check the code for ALX School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char buffer[98] = {0x00};
+	char *buffer;
 
+	buffer = create_array(98, 'H');
+	if  (buffer == NULL)
+	{
+	    printf("failed to allocate memory\n");
+	    return (1);
+	}
 	simple_print_buffer(buffer, 98);
-	_memset(buffer, 0x01, 95);
-	printf("-------------------------------------------------\n");
-	simple_print_buffer(buffer, 98);    
+	free(buffer);
 	return (0);
 }
--- main.h
+++ main.h
@@ -1,17 +1,13 @@
 #ifndef MAIN_H
-int _putchar(char c);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
-void print_diagsums(int *a, int size);
-char *_strstr(char *haystack, char *needle);
-void set_string(char **s, char *to);
-
-
-
 #define MAIN_H
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
+
+
 #endif /*MAIN_H*/
